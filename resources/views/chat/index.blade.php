a929a657c606f2a789277129cf405ddba3daa4ed
jhonrymat <jhonrymat@gmail.com>
2024-04-27 09:59:38 -0500

    agregando iframe cannabito

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -5,10 +5,10 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-cannabito />
+    <div class="py-12" style="height: 100vh;">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" style="height: 100%;">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg" style="height: 100%;">
+                <iframe src="https://cannabito-ia-front-production.up.railway.app/" style="width: 100%; height: 100%; border: 0;"></iframe>
             </div>
         </div>
     </div>
